00c71ba1ab672e8f6f6c96b6614e34fe4cbf810d
acbonill <acbonill@bancolombia.com.co>
2025-02-14 01:55:20 -0500

    init: initialize SolarApp project

---
+++ src/test/java/com/rayitosdesol/solarapp/SolarappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rayitosdesol.solarapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
