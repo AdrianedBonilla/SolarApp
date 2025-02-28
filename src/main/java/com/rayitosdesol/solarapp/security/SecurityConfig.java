414fad4cdcd888a8ade2b8c51dfb38b69956983b
IngFabian97 <fabian.guarin@hotmail.com>
2025-02-28 07:21:51 -0500

    Add auth0

--- src/main/java/com/rayitosdesol/solarapp/security/SecurityConfig.java
+++ src/main/java/com/rayitosdesol/solarapp/security/SecurityConfig.java
@@ -1,3 +1,4 @@
+// filepath: /D:/Talento tech proyecto/SolarApp/src/main/java/com/rayitosdesol/solarapp/security/SecurityConfig.java
 package com.rayitosdesol.solarapp.security;
 
 import org.springframework.context.annotation.Bean;
@@ -5,20 +6,34 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/v1/**").authenticated() 
-                .anyRequest().permitAll()
-            )
-            .httpBasic(withDefaults());
+        http.csrf().disable()
+            .authorizeHttpRequests()
+            .requestMatchers("/api/v1/auth/**").permitAll()
+            .requestMatchers("/api/v1/**").authenticated()
+            .and()
+            .oauth2ResourceServer()
+            .jwt()
+            .jwtAuthenticationConverter(jwtAuthenticationConverter());
         return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("permissions");
+        grantedAuthoritiesConverter.setAuthorityPrefix("");
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
     }
 
     @Bean
