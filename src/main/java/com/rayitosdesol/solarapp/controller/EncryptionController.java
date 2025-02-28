5136f718d7bffcef327676db972a02f00dfb712b
IngFabian97 <fabian.guarin@hotmail.com>
2025-02-27 23:29:04 -0500

    Service encryption

--- src/main/java/com/rayitosdesol/solarapp/controller/EncryptionController.java
+++ src/main/java/com/rayitosdesol/solarapp/controller/EncryptionController.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/")
 public class EncryptionController {
 
     private final EncryptionService encryptionService;
